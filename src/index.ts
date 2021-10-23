058adf226e2eaf2b388d8903f2e58bdb7f78db04
Matheus2798 <matheus.s.f@outlook.com.br>
2021-10-22 22:12:06 -0300

    first deploy Heroku

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-import * as express from "express";
+import express from "express";
 import routes from "./routes";
-import * as cors from 'cors';
+import cors from 'cors';
 
 const app = express();
 createConnection();
@@ -11,4 +11,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes)
 
-app.listen(3333, () => { console.log(`🔥 Server on fire http://localhost:3333/`) })
+app.listen(process.env.PORT || 3333, () => { console.log(`🔥 Server on fire http://localhost:3333/`) })
